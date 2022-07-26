05cc15332c5b01889bd141b75d224cfab312a4bf
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-07-26 14:36:06 +0900

    おまけ(useMemo)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./components/ChildArea";
 import "./styles.css";
 
@@ -9,6 +9,9 @@ export const App = () => {
   const onChangeText = (e) => setText(e.target.value);
 
   const onClickOpen = () => setOpen(!open);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
 
   const onClickClose = useCallback(() => setOpen(false), [setOpen]);
 
