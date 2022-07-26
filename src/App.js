5cb17154beb7c348df6bca734317fff5e7c583d9
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-07-26 13:26:42 +0900

    レンダリング確認用の実装

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { useState } from "react";
+import { ChildArea } from "./components/ChildArea";
 import "./styles.css";
 
 export const App = () => {
   console.log("rendering");
 
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
 
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open);
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <p>{count}</p>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
     </div>
   );
 };
