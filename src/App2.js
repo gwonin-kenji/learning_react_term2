89c4b5aea0b4039857ba596c562ef2a81d5ac817
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-07-27 17:55:38 +0900

    スタイリング: Inline styleの当て方

---
+++ src/App2.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { InlineStyle } from "./components/InlineStyle";
+
+export default function App2() {
+  return (
+    <div className="App2">
+      <InlineStyle />
+    </div>
+  );
+}
