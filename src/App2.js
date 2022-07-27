027beed60205713424e3b87aacfb254c9566b7b4
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-07-27 18:16:26 +0900

    スタイリング: CSS Modules

--- src/App2.js
+++ src/App2.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 import { InlineStyle } from "./components/InlineStyle";
+import { CssModules } from "./components/CssModules";
 
 export default function App2() {
   return (
     <div className="App2">
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
