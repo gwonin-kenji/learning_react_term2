d6431252f3ad4db9c92abbaa35250742b058a638
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-07-27 18:43:20 +0900

    スタイリング: Styled JSX

--- src/App2.js
+++ src/App2.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import { InlineStyle } from "./components/InlineStyle";
 import { CssModules } from "./components/CssModules";
+import { StyledJsx } from "./components/StyledJsx";
 
 export default function App2() {
   return (
     <div className="App2">
       <InlineStyle />
       <CssModules />
+      <StyledJsx />
     </div>
   );
 }
