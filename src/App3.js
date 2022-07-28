e2c1eeaabbbdb795678c22e15b7f8fc5022772cf
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-07-28 15:42:30 +0900

    ベースコード準備 (React Router: ルーティングの基礎)

---
+++ src/App3.js
@@ -0,0 +1,14 @@
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
+import "./styles.css";
+
+export default function App3() {
+  return (
+    <div className="App3">
+      <Home />
+      <Page1 />
+      <Page2 />
+    </div>
+  );
+}
