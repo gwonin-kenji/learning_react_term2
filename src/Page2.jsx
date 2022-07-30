d0e7df58c0ed867a8b2c3af393e97e56d4224d0a
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-07-30 17:10:35 +0900

    URLパラメータを扱う

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export const Page2 = () => {
   return (
     <div>
       <h1>Page2 page</h1>
+      <Link to="/page2/222">Url Parameter</Link>
     </div>
   );
 };
